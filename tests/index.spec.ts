9feb469159f6ed4a30c006d45898ae6c2476427c
Vajahath Ahmed <vajuoff.1@gmail.com>
2020-01-18 15:47:21 +0530

    tests(): add url and path tests

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,7 +1,46 @@
-// import { add } from '../dist/index';
+const { startServerWithSwaggerFile } = require('../dist/index');
+import { join } from 'path';
+import got from 'got';
+import { Server } from 'http';
 
-// describe('testing add function', () => {
-//   test('5 + 6 = 11', () => {
-//     expect(add(5, 6)).toBe(11);
-//   });
-// });
+let theServer: Server;
+
+test('testing base function with file path', async done => {
+  const { port, server } = await startServerWithSwaggerFile(
+    join(__dirname, 'swagger.json')
+  );
+
+  theServer = server;
+
+  got(`http://localhost:${port}/swagger-doc`)
+    .then(resp => {
+      if (resp?.statusCode && resp?.statusCode < 400) {
+        return done();
+      }
+      throw new Error('invalid status code');
+    })
+    .catch(err => {
+      throw err;
+    });
+});
+
+test('testing base function with url', async done => {
+  const { port, server } = await startServerWithSwaggerFile(
+    'https://petstore.swagger.io/v2/swagger.json'
+  );
+
+  theServer = server;
+
+  got(`http://localhost:${port}/swagger-doc`)
+    .then(resp => {
+      if (resp?.statusCode && resp?.statusCode < 400) {
+        return done();
+      }
+      throw new Error('invalid status code');
+    })
+    .catch(err => {
+      throw err;
+    });
+});
+
+afterEach(done => theServer.close(err => (err ? done(err) : done())));
